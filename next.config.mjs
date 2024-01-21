9549f7faee1fbffd3a836bc119a8162d06a33029
Enol Casielles <enolcasielles@gmail.com>
2024-01-21 18:00:33 +0100

    build

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,7 @@
-import postgres from 'postgres';
-
-export const sql = postgres(process.env.POSTGRES_URL, {
-  ssl: 'allow',
-});
-
 const nextConfig = {
   experimental: {
     ppr: true,
     useLightningcss: true,
-  },
-  async redirects() {
-    if (!process.env.POSTGRES_URL) {
-      return [];
-    }
-
-    let redirects = await sql`
-      SELECT source, destination, permanent
-      FROM redirects;
-    `;
-
-    return redirects.map(({ source, destination, permanent }) => ({
-      source,
-      destination,
-      permanent: !!permanent,
-    }));
   },
   headers() {
     return [
